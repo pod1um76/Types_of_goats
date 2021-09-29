4f075f7a21f25840fa97e8615895e561762753ff
pod1um76 <lynant77@gmailcom>
2021-09-29 13:00:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/typesofgoats/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.typesofgoats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
